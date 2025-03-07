abacbd927bf754895f1212bc9d6e2cec20ebba8b
Klejdi <klejdiwork@gmail.com>
2025-03-07 21:43:18 +0100

    add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/ Menu";
+
+
+const router =createBrowserRouter([
+    {
+        path:"/",
+        element:<Home />,
+    },
+
+    {
+        path:"/menu",
+        element:<Menu />,
+    },
+    {
+            path:"*",
+            element:"error",
+    },
+],
+    {
+        basename:"/fast-pizza",
+    }
+
+);
 
 function App(){
 
-    return(
-    <div>
-        test
-    </div>
-    )
+    return <RouterProvider future={{v7_startTransition: true,v7_relativeSplatPath: true,}} router={router}/>
 }
 export default App;
